d5ec161736720859b05da4e1f4a80339ead648d1
BBAKJUN <wnsguddl789@gmail.com>
2023-06-24 22:14:06 +0900

    feat : 카카오 스크립트 초기화

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <div id="portal"></div>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<div id="portal"></div>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
