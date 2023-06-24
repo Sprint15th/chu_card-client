5982b1f4856afd7011483f0e4ec45ffbd7bad47a
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-06-24 17:41:20 +0900

    style: global css 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import PageTemplate from "@/components/PageTemplate";
+import "@/styles/global.css";
+
+function setScreenSize() {
+  const vh = window.innerHeight * 0.01;
+
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    setScreenSize();
+  }, []);
+
+  useEffect(() => {
+    const onResize = () => {
+      setScreenSize();
+    };
+
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
   return (
     <PageTemplate>
       <Component {...pageProps} />
