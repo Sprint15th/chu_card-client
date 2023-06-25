8213930996a8a899b0e1f66a52a16294df9588f0
anonymousrecords <a.Records2022@gmail.com>
2023-06-25 12:06:10 +0900

    Implement : 랜딩페이지 UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,18 +21,42 @@ const LandingPage = () => {
     router.push("/cake");
   };
 
-  const slideStyles = {
+  interface SlideStyles {
+    backgroundColor?: string;
+    width: string;
+    height: string;
+    display: string;
+    marginTop: string;
+    alignItems: string;
+    flexDirection?: "row" | "row-reverse" | "column" | "column-reverse";
+  }
+  
+  const slideStyles: SlideStyles = {
     width: "100%",
     height: "100vh",
     display: "flex",
-    justifyContent: "center",
+    marginTop: "10.063rem",
     alignItems: "center",
+    flexDirection: "column",
   };
 
   const slides = [
-    { backgroundColor: "teal", text: "red" },
-    { backgroundColor: "tomato", text: "blue" },
-    { backgroundColor: "skyblue", text: "시작하기" },
+    { 
+      img: '/landing/intro1.svg',
+      text: `친구가 생일인데 색다르게 
+축하해주고 싶지 않나요?`
+    },
+    { 
+      img: '/landing/Intro2.png',
+      text: `나만의 케이크를 제작하여
+친구를 감동시켜주세요!`,
+      comment: '그럼 시작해볼까요?'
+    },
+    { 
+      img: '/landing/intro3.svg',
+      backgroundColor: "white", 
+      text: "시작하기" 
+    },
   ];
 
   // 초기 슬라이드가 설정되기 전에는 캐러셀이나 다른 컨텐츠가 렌더링 방지
@@ -41,7 +65,7 @@ const LandingPage = () => {
   }
 
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <div style={{ width: "23.485rem", height: "100vh" }}>
       <Swiper
         initialSlide={initialSlide}
         modules={[Pagination]}
@@ -55,11 +79,13 @@ const LandingPage = () => {
             <div
               style={{ ...slideStyles, backgroundColor: slide.backgroundColor }}
             >
+              <img src={slide.img}/>
               {slide.text === "시작하기" ? (
                 <button onClick={handleStart}>{slide.text}</button>
               ) : (
                 slide.text
-              )}
+              )} <br/><br/>
+              {slide.comment}
             </div>
           </SwiperSlide>
         ))}
