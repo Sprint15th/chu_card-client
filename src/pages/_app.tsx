5801fa0b20ea21ed5b674cfa805d46bed4245c95
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-06-25 16:46:05 +0900

    style: 페이지 레이아웃 통일

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,38 @@
-import { useEffect } from "react";
-import { RecoilRoot } from "recoil";
-import type { AppProps } from "next/app";
-import PageTemplate from "@/components/PageTemplate";
-import "@/styles/global.css";
-import "@/styles/swiper.css";
+import { useEffect } from 'react';
+import { RecoilRoot } from 'recoil';
+import type { AppProps } from 'next/app';
+import PageTemplate from '@/components/PageTemplate';
+import '@/styles/global.css';
+import '@/styles/swiper.css';
 
-import { useKakaoScript } from "react-kakao-share";
+import { useKakaoScript } from 'react-kakao-share';
 
 export default function App({ Component, pageProps }: AppProps) {
-	useResizeEventHandler();
-	useKakaoScript();
-	return (
-		<RecoilRoot>
-			<PageTemplate>
-				<Component {...pageProps} />
-			</PageTemplate>
-		</RecoilRoot>
-	);
+  useResizeEventHandler();
+  useKakaoScript();
+  return (
+    <RecoilRoot>
+      <PageTemplate>
+        <Component {...pageProps} />
+      </PageTemplate>
+    </RecoilRoot>
+  );
 }
 
 const useResizeEventHandler = () => {
-	useEffect(() => {
-		if (typeof window === "undefined") return;
-		setScreenSize();
-		window.addEventListener("resize", setScreenSize);
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+    setScreenSize();
+    window.addEventListener('resize', setScreenSize);
 
-		return () => {
-			window.removeEventListener("resize", setScreenSize);
-		};
-	}, []);
+    return () => {
+      window.removeEventListener('resize', setScreenSize);
+    };
+  }, []);
 };
 
 function setScreenSize() {
-	const vh = window.innerHeight * 0.01;
+  const vh = window.innerHeight * 0.01;
 
-	document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
