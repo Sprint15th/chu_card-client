40b77c418a684aaa4400e18b4d69eb4155100a7d
BBAK_JUN <33216504+wnsguddl789@users.noreply.github.com>
2023-06-25 15:45:18 +0900

    feat : prisma api (#20)

---
+++ src/pages/api/cake/[cakeId].ts
@@ -0,0 +1,19 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/utils/prismaClient";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	switch (req.method) {
+		case "GET": {
+			const data = await prisma.cake.findUnique({
+				where: {
+					cakeId: Number(req.query.cakeId),
+				},
+			});
+
+			if (!data) res.json({ ok: false });
+
+			res.json({ ok: true, data });
+			break;
+		}
+	}
+}
