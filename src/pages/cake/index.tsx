7512f85772b84462c0c3a43af0fabf26a6473001
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-06-25 15:21:07 +0900

    feat: 서버 요청시 보여질 선물 애니메이션

--- src/pages/cake/index.tsx
+++ src/pages/cake/index.tsx
@@ -6,14 +6,23 @@ import TopNavigationBar from '@/components/TopNavigationBar';
 import { cakeState } from '@/store/cakeState';
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import styled from '@emotion/styled';
+import GiftBox from '@/components/GiftBox';
+import { useRouter } from 'next/navigation';
 
 const Cake = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { selectedIndex } = useRecoilValue(cakeState);
+  const [showGift, setShowGift] = useState(false);
+  const router = useRouter();
 
   const handleConfirm = () => {
     //TODO: 서버에 저장하는 로직
     setIsModalOpen(false);
+    setShowGift(true);
+    setTimeout(() => {
+      router.push('/cake/complete/11');
+    }, 3000);
   };
 
   const getComponentByStep = (step: number) => {
@@ -29,19 +38,27 @@ const Cake = () => {
     }
   };
 
+  if (showGift)
+    return (
+      <GiftContainer>
+        <GiftBox text='소중한분에게 선물이 배달되고 있어요.'></GiftBox>
+      </GiftContainer>
+    );
+
   return (
     <div style={{ padding: '16px' }}>
       <TopNavigationBar selectedIndex={selectedIndex} />
       <main>{getComponentByStep(selectedIndex)}</main>
       {isModalOpen && (
-        <ConfirmModal
-          onConfirm={handleConfirm}
-          closeModal={() => setIsModalOpen(false)}
-          selector='#portal'
-        />
+        <ConfirmModal onConfirm={handleConfirm} closeModal={() => setIsModalOpen(false)} selector='#portal' />
       )}
     </div>
   );
 };
 
 export default Cake;
+
+const GiftContainer = styled.div`
+  background-color: #ffa0a0;
+  min-height: calc(var(--vh, 1vh) * 100);
+`;
