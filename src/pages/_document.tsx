b4d68289f358a2c37de1dcc5a3c24568e310e3df
BBAKJUN <wnsguddl789@gmail.com>
2023-06-25 20:47:57 +0900

    chore : 이미지 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-	return (
-		<Html lang="en">
-			<Head />
-			<body>
-				<div id="portal"></div>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang='en'>
+      <Head>
+        <title>{'츄카드'}</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='description' content={'추카 받고싶지않나요?'} />
+        <meta property='og:title' content={'츄카드'} />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content={'https://chu-card-client.vercel.app/'} />
+        <meta property='og:image' content={'/logo.png'} />
+        <meta property='og:article:author' content='츄카드' />
+      </Head>
+      <body>
+        <div id='portal'></div>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
